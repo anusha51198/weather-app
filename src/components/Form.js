61386838d2d3f2b4f6d6cd5f04df1d3948fd6c54
201651027@iiitvadodara.ac.in <201651027@iiitvadodara.ac.in>
2019-05-25 16:33:16 +0530

    Added

---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Form(props){
+    return(
+      <div>
+        <form onSubmit={props.getWeather}>
+          <input type="text" name="city" placeholder="City..."/>
+          <input type="text" name="country" placeholder="Country..."/>
+          <button>Get Weather</button>
+        </form>
+      </div>
+    )
+  }
+
+
+export default Form
---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Titles(){
+    return(
+      <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <p className="title-container__subtitle">Find out Temperature, Conditions and more....</p>
+      </div>
+    )
+  }
+
+
+export default Titles
